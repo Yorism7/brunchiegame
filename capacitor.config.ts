870747d613a810209bb436d3c3770bd8b785b3d2
Chama kongsang <ddev.chama@gmail.com>
2024-05-21 15:19:44 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.brunchiegame',
+  appName: 'brunchiegame',
+  webDir: 'dist'
+};
+
+export default config;
