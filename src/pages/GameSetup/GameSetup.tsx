abbacf24f000fea4934a88ce3142c8f3efc4e87d
Chama kongsang <ddev.chama@gmail.com>
2024-06-13 01:49:14 +0700

    เลือกหมวกจอย page

--- src/pages/GameSetup/GameSetup.tsx
+++ src/pages/GameSetup/GameSetup.tsx
@@ -1,44 +1,93 @@
-import {  IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonIcon, IonImg, IonLabel, IonPage,  IonRow,  IonTabBar, IonTabButton  } from '@ionic/react';
+import React, { useState } from 'react';
+import {
+  IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid,
+  IonImg, IonInput, IonItem, IonLabel, IonList, IonPage, IonRow, useIonViewDidEnter
+} from '@ionic/react';
 import MyFooter from "../../components/MyFooter/MyFooter";
-
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-import './style.css';
-// import required modules
 import { EffectCards } from 'swiper/modules';
-
-
+import './style.css';
 
 const GameSetup: React.FC = () => {
+  const [questionCount, setQuestionCount] = useState<number>(0);
+
+  const incrementCount = () => {
+    console.log('Increment button clicked');
+    setQuestionCount(questionCount + 10);
+  };
+
+  const decrementCount = () => {
+    console.log('Decrement button clicked');
+    if (questionCount > 0) {
+      setQuestionCount(questionCount - 10);
+    }
+  };
+
+  useIonViewDidEnter(() => {
+    console.log('IonViewDidEnter: Page has fully loaded');
+  });
+
   return (
     <IonPage>
-      <IonContent color={'main'}>
+      <IonContent color='main'>
         <IonGrid>
-
           <IonRow>
-            <IonImg src='public\icon\KJKJ_LOGO.png'></IonImg>
+            <IonImg src='/icon/2-1.png'></IonImg>
           </IonRow>
           <IonRow>
-            <IonCol size="12">
+            <IonCol size="12" className='cardContainer'>
+              <IonImg className="topIcon" src='/icon/2-2.png'></IonImg>
               <Swiper
                 effect={'cards'}
                 grabCursor={true}
                 modules={[EffectCards]}
                 className="mySwiper"
+                observer={true}
+                observeParents={true}
               >
-                <SwiperSlide><IonImg src='public\icon\KJKJ_LOGO.png'></IonImg></SwiperSlide>
-                <SwiperSlide><IonImg src='public\icon\KJKJ_LOGO.png'></IonImg></SwiperSlide>
-                <SwiperSlide><IonImg src='public\icon\KJKJ_LOGO.png'></IonImg></SwiperSlide>
-                <SwiperSlide><IonImg src='public\icon\KJKJ_LOGO.png'></IonImg></SwiperSlide>
-                <SwiperSlide><IonImg src='public\icon\KJKJ_LOGO.png'></IonImg></SwiperSlide>
-                <SwiperSlide><IonImg src='public\icon\KJKJ_LOGO.png'></IonImg></SwiperSlide>
-                <SwiperSlide><IonImg src='public\icon\KJKJ_LOGO.png'></IonImg></SwiperSlide>
-                <SwiperSlide><IonImg src='public\icon\KJKJ_LOGO.png'></IonImg></SwiperSlide>
+                <SwiperSlide><IonImg src='/icon/KJKJ_LOGO.png'></IonImg></SwiperSlide>
+                <SwiperSlide><IonImg src='/icon/KJKJ_LOGO.png'></IonImg></SwiperSlide>
+                <SwiperSlide><IonImg src='/icon/KJKJ_LOGO.png'></IonImg></SwiperSlide>
+                <SwiperSlide><IonImg src='/icon/KJKJ_LOGO.png'></IonImg></SwiperSlide>
+                <SwiperSlide><IonImg src='/icon/KJKJ_LOGO.png'></IonImg></SwiperSlide>
+                <SwiperSlide><IonImg src='/icon/KJKJ_LOGO.png'></IonImg></SwiperSlide>
+                <SwiperSlide><IonImg src='/icon/KJKJ_LOGO.png'></IonImg></SwiperSlide>
+                <SwiperSlide><IonImg src='/icon/KJKJ_LOGO.png'></IonImg></SwiperSlide>
+                <SwiperSlide><IonImg src='/icon/KJKJ_LOGO.png'></IonImg></SwiperSlide>
               </Swiper>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol size="12">
+              <IonList className='designInput'>
+                <IonItem className='CounterButton'>
+                  <IonButton 
+                    shape='round' 
+                    className='addNumber' 
+                    color='base' 
+                    onClick={incrementCount}
+                  >
+                    +
+                  </IonButton>
+                  <IonInput 
+                    type="number" 
+                    value={questionCount}
+                    onIonChange={e => setQuestionCount(Number(e.detail.value))}
+                    disabled={true}
+                    placeholder="จำนวนคำถาม"
+                  ></IonInput>
+                  <IonButton 
+                    shape='round' 
+                    className='delNumber' 
+                    color='base' 
+                    onClick={decrementCount}
+                  >
+                    -
+                  </IonButton>
+                </IonItem>
+              </IonList>
             </IonCol>
           </IonRow>
           <IonRow>
@@ -46,10 +95,9 @@ const GameSetup: React.FC = () => {
               <IonButton expand='block' color="light" shape='round' fill='outline' href='/home'><b>กดค้างเพื่อค้นใจ</b></IonButton>
             </IonCol>
           </IonRow>
-
         </IonGrid>
-        </IonContent>
-        <MyFooter></MyFooter>
+      </IonContent>
+      <MyFooter />
     </IonPage>
   );
 };
