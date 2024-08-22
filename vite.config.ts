2a7fae984c4a5e46481fabbe6e6ed4606c4e1f6f
Chama kongsang <ddev.chama@gmail.com>
2024-08-22 12:33:00 +0700

    optimize compress img png jpg svg cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,48 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    ViteImageOptimizer({
+      /* pass your config */
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+              },
+              cleanupIDs: {
+                minify: true,
+                remove: true,
+              },
+              convertPathData: false,
+            },
+          }
+        ],
+      },
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 50,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 50,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 50,
+      },
+      cache: true,
+    }),
   ],
   test: {
     globals: true,
